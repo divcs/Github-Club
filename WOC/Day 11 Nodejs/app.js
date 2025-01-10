be6c8f6498a3bd945e4600da29e816da2cdf2f52
Divya <singhdivya10aug2002@gmail.com>
2025-01-11 00:17:17 +0530

    Nodejs path module

--- WOC/Day 11 Nodejs/app.js
+++ WOC/Day 11 Nodejs/app.js
@@ -1,7 +1,7 @@
 var url = 'http://mylearning/log' //Created a var url
 var name = 'Rohit' //Created a var name
 
-console.log(name);
+console.log(name)
 
 //Creating Function.
 function log(msg) {
@@ -11,10 +11,20 @@ function log(msg) {
 //Calling the Function.
 log('sorry')
 
-//Exports
+//Exports: 3 way to export the function.
+// 1 way
 module.exports.log = log
 module.exports.url = url
 module.exports.name = name
+
+// 2 way
+module.exports = log
+
+// 3 way
+exports.log = log
+
+//wrong way
+//export=log;
 
 //printing name
 console.log('My name is sumit')
--- WOC/Day 11 Nodejs/index.js
+++ WOC/Day 11 Nodejs/index.js
@@ -1,4 +1,4 @@
-var h1 = require('./app.js')
+const h1 = require('./app.js')
 h1.name = 'Divya'
 console.log(h1.name)
 // logger.log('sorry from index.js')
