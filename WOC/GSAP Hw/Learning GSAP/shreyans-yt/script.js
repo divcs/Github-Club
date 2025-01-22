aab08df46837712c93732ff2e36c64a598c54137
Divya <singhdivya10aug2002@gmail.com>
2025-01-22 21:37:19 +0530

    gsap

--- WOC/GSAP Hw/Learning GSAP/shreyans-yt/script.js
+++ WOC/GSAP Hw/Learning GSAP/shreyans-yt/script.js
@@ -1,1 +1,21 @@
-gsap.to('#box', { x: 1000, duration: 2, delay: 1 })
+gsap.to('#box1', {
+  x: 1200,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: 'blue',
+  borderRadius: '50%',
+  scale: 0.5,
+})
+gsap.from('#box2', { x: 1000, duration: 2, delay: 1 })
+
+gsap.fromTo('#box2', { x: 1000, duration: 2, delay: 1 })
+
+// stagger se sare h1 ko ek ek karke animate karega
+gsap.from('h1', {
+  opacity: 0,
+  duration: 1,
+  y: 30,
+  delay: 0.2,
+  stagger: 0.5,
+})
