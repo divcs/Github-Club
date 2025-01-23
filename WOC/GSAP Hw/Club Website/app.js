302712230c49016cc480dfa4874c06867aa0feae
Divya <singhdivya10aug2002@gmail.com>
2025-01-23 19:36:58 +0530

    club website

---
+++ WOC/GSAP Hw/Club Website/app.js
@@ -0,0 +1,8 @@
+// Add this to a separate JS file or include it in the HTML <script> block
+// GSAP animation for the BaseClub logo
+gsap.from('.baseclub-logo', {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  ease: 'power2.out',
+})
